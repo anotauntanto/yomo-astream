14178a4fcf2e061a42d771f2cc7631f303dbb1e0
Cise Midoglu <cise@simula.no>
2017-08-04 14:50:48 +0200

    Run docker script.

--- astream/monroe/astream_start.sh
+++ astream/monroe/astream_start.sh
@@ -2,6 +2,6 @@
 
 INTERFACE='eth0'
 
-tshark -n -i $INTERFACE -E separator=, -T fields -e frame.time_epoch -e tcp.len -e frame.len -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.analysis.ack_rtt -e tcp.analysis.lost_segment -e tcp.analysis.out_of_order -e tcp.analysis.fast_retransmission -e tcp.analysis.duplicate_ack -e dns -Y 'tcp or dns'  >> /monroe/results/astream_tshark_$(date +%Y-%m-%d_%H-%M-%S).txt 2> /monroe/results/astream_tshark_error_$(date +%Y-%m-%d_%H-%M-%S).txt &
+$(which tshark) -n -i $INTERFACE -E separator=, -T fields -e frame.time_epoch -e tcp.len -e frame.len -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.analysis.ack_rtt -e tcp.analysis.lost_segment -e tcp.analysis.out_of_order -e tcp.analysis.fast_retransmission -e tcp.analysis.duplicate_ack -e dns -Y 'tcp or dns'  >> /monroe/results/astream_tshark_$(date +%Y-%m-%d_%H-%M-%S).txt 2> /monroe/results/astream_tshark_error_$(date +%Y-%m-%d_%H-%M-%S).txt &
 
 /usr/bin/python /opt/monroe/AStream_MONROE.py
