8c0a93a1adbca7f0d82147cec207d01b410c80d9
Cise Midoglu <cise@simula.no>
2017-09-25 14:22:53 +0200

    Start.py typo fix.

--- videomon_docker/start.py
+++ videomon_docker/start.py
@@ -258,7 +258,7 @@ def run_exp(meta_info, expconfig):
             "cnf_video_id": cfg['cnf_video_id'],
             "TEMPOUTPUT_AStream": "NA",
             "TEMPOUTPUT_YoMo": "NA",
-            "NodeId": cfg['nodeid',
+            "NodeId": cfg['nodeid'],
             "cnf_yomo_playback_duration_s": cfg["cnf_yomo_playback_duration_s"]]
         })
         print('DBG: testpoint2')
