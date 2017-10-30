85c5068edc6e27b892245dc4b537e89f7382f066
Cise Midoglu <cise@simula.no>
2017-10-30 17:10:08 +0100

    Analysis script draft (combo).

---
+++ utils/analysis_scripts/analysis_combo.m
@@ -0,0 +1,329 @@
+
+%% Configuration
+
+filename = 'test.json';
+font_size = 14;
+
+ax1_position = [0 0 1 1];
+ax2_position = [.45 .1 .5 .8];
+
+textbox_x1 = .025;
+textbox_y1 = 0.7;
+textbox_x2 = .025;
+textbox_y2 = 0.35;
+
+bitrates_delimiter = ';';
+
+%% Reading Input
+
+filetext = fileread(filename);
+jsonvalue = jsondecode(filetext);
+
+cnf_tag = get_field_str(jsonvalue,'cnf_tag');
+container_version = get_field_str(jsonvalue,'ContainerVersion');
+data_id = get_field_str(jsonvalue,'DataId');
+node_id = get_field_str(jsonvalue,'NodeId');
+timestamp = get_field_str(jsonvalue,'Time');
+
+mcc_mnc_nw = get_field_num(jsonvalue,'NWMCCMNC');
+mcc_mnc_sim = get_field_num(jsonvalue,'IMSIMCCMNC');
+cnf_astream_segment_limit = get_field_num(jsonvalue,'cnf_astream_segment_limit');
+cnf_yomo_playback_duration_s = get_field_num(jsonvalue,'cnf_yomo_playback_duration_s');
+
+cnf_q1 = get_field_num(jsonvalue,'cnf_q1');
+cnf_q2 = get_field_num(jsonvalue,'cnf_q2');
+cnf_q3 = get_field_num(jsonvalue,'cnf_q3');
+cnf_q4 = get_field_num(jsonvalue,'cnf_q4');
+
+res_astream_available_bitrates = get_field_str(jsonvalue,'res_astream_available_bitrates');
+
+res_yomo_bitrate_mean = get_field_num(jsonvalue,'res_yomo_bitrate_mean');
+res_yomo_bitrate_max = get_field_num(jsonvalue,'res_yomo_bitrate_max');
+res_yomo_bitrate_min = get_field_num(jsonvalue,'res_yomo_bitrate_min');
+res_yomo_bitrate_q1 = get_field_num(jsonvalue,'res_yomo_bitrate_q1');
+res_yomo_bitrate_q2 = get_field_num(jsonvalue,'res_yomo_bitrate_q2');
+res_yomo_bitrate_q3 = get_field_num(jsonvalue,'res_yomo_bitrate_q3');
+res_yomo_bitrate_q4 = get_field_num(jsonvalue,'res_yomo_bitrate_q4');
+
+res_astream_bitrate_mean = get_field_num(jsonvalue,'res_astream_bitrate_mean');
+res_astream_bitrate_max = get_field_num(jsonvalue,'res_astream_bitrate_max');
+res_astream_bitrate_min = get_field_num(jsonvalue,'res_astream_bitrate_min');
+res_astream_bitrate_q1 = get_field_num(jsonvalue,'res_astream_bitrate_q1');
+res_astream_bitrate_q2 = get_field_num(jsonvalue,'res_astream_bitrate_q2');
+res_astream_bitrate_q3 = get_field_num(jsonvalue,'res_astream_bitrate_q3');
+res_astream_bitrate_q4 = get_field_num(jsonvalue,'res_astream_bitrate_q4');
+
+res_yomo_buffer_mean = get_field_num(jsonvalue,'res_yomo_buffer_mean');
+res_yomo_buffer_max = get_field_num(jsonvalue,'res_yomo_buffer_max');
+res_yomo_buffer_min = get_field_num(jsonvalue,'res_yomo_buffer_min');
+res_yomo_buffer_q1 = get_field_num(jsonvalue,'res_yomo_buffer_q1');
+res_yomo_buffer_q2 = get_field_num(jsonvalue,'res_yomo_buffer_q2');
+res_yomo_buffer_q3 = get_field_num(jsonvalue,'res_yomo_buffer_q3');
+res_yomo_buffer_q4 = get_field_num(jsonvalue,'res_yomo_buffer_q4');
+
+res_astream_buffer_mean = get_field_num(jsonvalue,'res_astream_buffer_mean');
+res_astream_buffer_max = get_field_num(jsonvalue,'res_astream_buffer_max');
+res_astream_buffer_min = get_field_num(jsonvalue,'res_astream_buffer_min');
+res_astream_buffer_q1 = get_field_num(jsonvalue,'res_astream_buffer_q1');
+res_astream_buffer_q2 = get_field_num(jsonvalue,'res_astream_buffer_q2');
+res_astream_buffer_q3 = get_field_num(jsonvalue,'res_astream_buffer_q3');
+res_astream_buffer_q4 = get_field_num(jsonvalue,'res_astream_buffer_q4');
+
+res_yomo_numstalls = get_field_num(jsonvalue,'res_yomo_numstalls');
+res_yomo_durstalls_mean = get_field_num(jsonvalue,'res_yomo_durstalls_mean');
+res_yomo_durstalls_max = get_field_num(jsonvalue,'res_yomo_durstalls_max');
+res_yomo_durstalls_min = get_field_num(jsonvalue,'res_yomo_durstalls_min');
+res_yomo_durstalls_q1 = get_field_num(jsonvalue,'res_yomo_durstalls_q1');
+res_yomo_durstalls_q2 = get_field_num(jsonvalue,'res_yomo_durstalls_q2');
+res_yomo_durstalls_q3 = get_field_num(jsonvalue,'res_yomo_durstalls_q3');
+res_yomo_durstalls_q4 = get_field_num(jsonvalue,'res_yomo_durstalls_q4');
+
+res_astream_numstalls = get_field_num(jsonvalue,'res_astream_numstalls');
+res_astream_durstalls_mean = get_field_num(jsonvalue,'res_astream_durstalls_mean');
+res_astream_durstalls_max = get_field_num(jsonvalue,'res_astream_durstalls_max');
+res_astream_durstalls_min = get_field_num(jsonvalue,'res_astream_durstalls_min');
+res_astream_durstalls_q1 = get_field_num(jsonvalue,'res_astream_durstalls_q1');
+res_astream_durstalls_q2 = get_field_num(jsonvalue,'res_astream_durstalls_q2');
+res_astream_durstalls_q3 = get_field_num(jsonvalue,'res_astream_durstalls_q3');
+res_astream_durstalls_q4 = get_field_num(jsonvalue,'res_astream_durstalls_q4');
+
+res_yomo_numswitches_up = get_field_num(jsonvalue,'res_yomo_numswitches_up');
+res_yomo_numswitches_down = get_field_num(jsonvalue,'res_yomo_numswitches_down');
+res_astream_numswitches_up = get_field_num(jsonvalue,'res_astream_numswitches_up');
+res_astream_numswitches_down = get_field_num(jsonvalue,'res_astream_numswitches_down');
+
+%% Plots
+
+close all
+
+if isnan(cnf_q1) || isnan(cnf_q2) || isnan(cnf_q3) || isnan(cnf_q4)
+    str_q1 = 'Q1';
+    str_q2 = 'Q2';
+    str_q3 = 'Q3';
+    str_q4 = 'Q4';
+else
+    str_q1 = ['Q1: ',num2str(cnf_q1)];
+    str_q2 = ['Q2: ',num2str(cnf_q2)];
+    str_q3 = ['Q3: ',num2str(cnf_q3)];
+    str_q4 = ['Q4: ',num2str(cnf_q4)];
+end
+
+descr_bitrates = strsplit(res_astream_available_bitrates,bitrates_delimiter);
+
+%% Plots: Bitrate
+
+figure;
+
+descr = {['Time: ',timestamp],...%strcat('Time: ',timestamp);
+    ['Container Version: ',container_version],...
+    ['Node ID: ',node_id],...
+    ['MCCMNC (SIM): ',num2str(mcc_mnc_sim)],...
+    ['MCCMNC (NW): ',num2str(mcc_mnc_nw)],...
+    ['AStream Segment Limit: ',num2str(cnf_astream_segment_limit)],...
+    ['YoMo Playback Duration: ',num2str(cnf_yomo_playback_duration_s)],...
+    '',...
+    'Available Bitrates: '};
+
+yomo_stacked = [res_yomo_bitrate_mean;
+    res_yomo_bitrate_max;
+    res_yomo_bitrate_min;
+    res_yomo_bitrate_q1;
+    res_yomo_bitrate_q2;
+    res_yomo_bitrate_q3;
+    res_yomo_bitrate_q4];
+
+astream_stacked = [res_astream_bitrate_mean;
+    res_astream_bitrate_max;
+    res_astream_bitrate_min;
+    res_astream_bitrate_q1;
+    res_astream_bitrate_q2;
+    res_astream_bitrate_q3;
+    res_astream_bitrate_q4];
+
+ax1 = axes('position',ax1_position,'Visible','off');
+ax2 = axes('Position',ax2_position);
+
+bar(ax2,[yomo_stacked,astream_stacked])
+xticklabels({'mean','max','min',str_q1,str_q2,str_q3,str_q4});
+h = legend('YoMo','AStream');
+legend('show')
+set(gca,'FontSize',font_size)
+set(h,'FontSize',font_size)
+
+title_str = 'Bitrate (Kbit/s)';
+title(title_str);
+
+axes(ax1)
+t1 = text(textbox_x1,textbox_y1,descr);
+t1.FontSize = font_size;
+
+axes(ax1)
+t2 = text(textbox_x2,textbox_y2,descr_bitrates);
+t2.FontSize = font_size;
+
+%% Plots: Buffer
+
+figure;
+
+descr = {['Time: ',timestamp],...
+    ['Container Version: ',container_version],...
+    ['Node ID: ',node_id],...
+    ['MCCMNC (SIM): ',num2str(mcc_mnc_sim)],...
+    ['MCCMNC (NW): ',num2str(mcc_mnc_nw)],...
+    ['AStream Segment Limit: ',num2str(cnf_astream_segment_limit)],...
+    ['YoMo Playback Duration: ',num2str(cnf_yomo_playback_duration_s)]};
+
+yomo_stacked = [res_yomo_buffer_mean;
+    res_yomo_buffer_max;
+    res_yomo_buffer_min;
+    res_yomo_buffer_q1;
+    res_yomo_buffer_q2;
+    res_yomo_buffer_q3;
+    res_yomo_buffer_q4];
+
+astream_stacked = [res_astream_buffer_mean;
+    res_astream_buffer_max;
+    res_astream_buffer_min;
+    res_astream_buffer_q1;
+    res_astream_buffer_q2;
+    res_astream_buffer_q3;
+    res_astream_buffer_q4];
+
+ax1 = axes('Position',ax1_position,'Visible','off');
+ax2 = axes('Position',ax2_position);
+
+bar(ax2,[yomo_stacked,astream_stacked])
+xticklabels({'mean','max','min',str_q1,str_q2,str_q3,str_q4});
+h = legend('YoMo','AStream');
+legend('show')
+set(gca,'FontSize',font_size)
+set(h,'FontSize',font_size)
+
+title_str = 'Buffer (s)';
+title(title_str);
+
+axes(ax1)
+t1 = text(textbox_x1,textbox_y1,descr);
+t1.FontSize = font_size;
+
+%% Plots: Duration of Stalls
+
+figure;
+
+descr = {['Time: ',timestamp],...%strcat('Time: ',timestamp);
+    ['Container Version: ',container_version],...
+    ['Node ID: ',node_id],...
+    ['MCCMNC (SIM): ',num2str(mcc_mnc_sim)],...
+    ['MCCMNC (NW): ',num2str(mcc_mnc_nw)],...
+    ['AStream Segment Limit: ',num2str(cnf_astream_segment_limit)],...
+    ['YoMo Playback Duration: ',num2str(cnf_yomo_playback_duration_s)],...
+    '',...
+    ['Number of Stalls (YoMo): ',num2str(res_yomo_numstalls)],...
+    ['Number of Stalls (AStream): ',num2str(res_astream_numstalls)]};
+
+yomo_stacked = [res_yomo_durstalls_mean;
+    res_yomo_durstalls_max;
+    res_yomo_durstalls_min;
+    res_yomo_durstalls_q1;
+    res_yomo_durstalls_q2;
+    res_yomo_durstalls_q3;
+    res_yomo_durstalls_q4];
+
+astream_stacked = [res_astream_durstalls_mean;
+    res_astream_durstalls_max;
+    res_astream_durstalls_min;
+    res_astream_durstalls_q1;
+    res_astream_durstalls_q2;
+    res_astream_durstalls_q3;
+    res_astream_durstalls_q4];
+
+ax1 = axes('Position',ax1_position,'Visible','off');
+ax2 = axes('Position',ax2_position);
+
+bar(ax2,[yomo_stacked,astream_stacked])
+xticklabels({'mean','max','min',str_q1,str_q2,str_q3,str_q4});
+h = legend('YoMo','AStream');
+legend('show')
+set(gca,'FontSize',font_size)
+set(h,'FontSize',font_size)
+
+title_str = 'Duration of Stalls (s)';
+title(title_str);
+
+axes(ax1)
+t1 = text(textbox_x1,textbox_y1,descr);
+t1.FontSize = font_size;
+
+%% Plots: Number of Switches
+figure;
+
+descr = {['Time: ',timestamp],...
+    ['Container Version: ',container_version],...
+    ['Node ID: ',node_id],...
+    ['MCCMNC (SIM): ',num2str(mcc_mnc_sim)],...
+    ['MCCMNC (NW): ',num2str(mcc_mnc_nw)],...
+    ['AStream Segment Limit: ',num2str(cnf_astream_segment_limit)],...
+    ['YoMo Playback Duration: ',num2str(cnf_yomo_playback_duration_s)],...
+    '',...
+    'Available Bitrates: '};
+
+yomo_stacked = [res_yomo_numswitches_up;
+    res_yomo_numswitches_down];
+
+astream_stacked = [res_astream_numswitches_up;
+    res_astream_numswitches_down];
+
+ax1 = axes('Position',ax1_position,'Visible','off');
+ax2 = axes('Position',ax2_position);
+
+bar(ax2,[yomo_stacked,astream_stacked])
+xticklabels({'up','down'});
+h = legend('YoMo','AStream');
+legend('show')
+set(gca,'FontSize',font_size)
+set(h,'FontSize',font_size)
+
+title_str = 'Number of Switches';
+title(title_str);
+
+axes(ax1)
+t1 = text(textbox_x1,textbox_y1,descr);
+t1.FontSize = font_size;
+
+axes(ax1)
+t2 = text(textbox_x2,textbox_y2,descr_bitrates);
+t2.FontSize = font_size;
+
+%% Functions
+
+function str_out = get_field_str(json_in,str_in)
+try
+    value_str = getfield(json_in,str_in);
+    if strcmp(value_str,'NA') || strcmp(value_str,'None') || isempty(value_str)
+        str_out='NA';
+    else
+        str_out = value_str;
+    end
+catch ME
+    str_out = 'NA';
+end
+end
+
+function num_out = get_field_num(json_in,str_in)
+try
+    value_str = getfield(json_in,str_in);
+    if ischar(value_str)
+        if strcmp(value_str,'NaN') || strcmp(value_str,'None') || isempty(value_str)
+            num_out = NaN;
+        else
+            num_out = str2num(value_str);
+        end
+        
+    elseif isnumeric(value_str)
+            num_out = value_str;
+    end
+catch ME
+    %disp(ME)
+    num_out = NaN;
+end
+end
