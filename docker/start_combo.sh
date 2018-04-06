44a605b45db65d8ba48739788565b89e2efc598b
Cise Midoglu <cise@simula.no>
2018-04-06 18:23:05 +0200

    Debug output.

--- docker/start_combo.sh
+++ docker/start_combo.sh
@@ -6,7 +6,12 @@ echo "  "
 echo "-----------------------------"
 echo "DBG: Running Nettest"
 echo "-----------------------------"
-
 python nettest.py
+
 wait 1
+
+echo "  "
+echo "-----------------------------"
+echo "DBG: Running VideoMon"
+echo "-----------------------------"
 python videomon_start.py
