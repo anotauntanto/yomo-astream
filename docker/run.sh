22c3a2ed3ae69e1f6277e5c6b0eef38a3b12196e
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2018-04-20 17:48:12 +0200

    added command line switch to run.sh, so that it is possible to bash into a running container: ./run.sh --bash

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,20 @@ CONFIG="$(pwd)/videomon_config"
 RESULT_DIR=$1
 RESULT_DIR=${RESULT_DIR:=/tmp} 
 
-docker run --shm-size=1g -v $RESULT_DIR:/monroe/results  -v $CONFIG:/monroe/config  -t $CONTAINER .
+
+if [[ $@ == **--bash** ]]; then
+	# try to open exec inside running container
+	ID=$(docker ps | grep $CONTAINER | head -n1 | awk '{ print $1}')
+	if [[ -z "$ID" ]]; then 
+		docker ps
+		echo -e "\nNo running container $CONTAINER found!\n Exiting...\n\n"
+		exit -1;
+	else
+		docker exec -i -t $ID /bin/bash
+	fi
+else 
+	# run the normal way
+	docker run --shm-size=1g -v $RESULT_DIR:/monroe/results  -v $CONFIG:/monroe/config  -t $CONTAINER .
+fi
+
+
